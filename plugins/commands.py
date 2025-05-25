eed45b0954992155efffeafb3e06e0dc6dc85070
pachax001 <pachax001@gmail.com>
2025-05-25 16:46:33 +0530

    .

--- plugins/commands.py
+++ plugins/commands.py
@@ -479,7 +479,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(info.PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, info.NON_PREMIUM_DAILY_LIMIT),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -556,7 +556,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(info.PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, info.NON_PREMIUM_DAILY_LIMIT),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
